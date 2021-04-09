4d1c6142b48ea0116c73680b8d42cfd677025cc6
Eugene <ondrinevg@gmail.com>
2021-04-09 14:29:53 +0300

    add comment to client

--- client/src/redux/saga/client/addCommentToClientSagaWatcher.js
+++ client/src/redux/saga/client/addCommentToClientSagaWatcher.js
@@ -2,13 +2,13 @@ import { call, put, takeLatest } from 'redux-saga/effects';
 import { addCommentToClient } from '../../actionCreators/clientAC';
 import { ADD_COMMENT_CLIENT_SAGA } from '../../types/clientTypes';
 
-const editClientToServer = ({text, id}) => {
+const addCommentClientToServer = ({text, id}) => {
   return fetch(`${process.env.REACT_APP_ADDRESS_TO_FETCH}/api/v1/clients/${id}/comments`, {
     method: 'POST',
     headers: {
       'Content-Type' : 'application/json',
     },
-    body: JSON.stringify(text),
+    body: JSON.stringify({text}),
   })
     .then(response => response.json())
 }
@@ -16,7 +16,7 @@ const editClientToServer = ({text, id}) => {
 
 function* clientSagaWorker(action) {
   try {
-    const client = yield call(editClientToServer, action.payload);
+    const client = yield call(addCommentClientToServer, action.payload);
     yield put(addCommentToClient(client));
   } catch (e) {
     yield put({ type: "USER_FETCH_FAILED", message: e.message });
