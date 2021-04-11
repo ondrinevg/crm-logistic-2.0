747d8ee3d3f44b61d0ff151a4754e93118ef6f99
Eugene <ondrinevg@gmail.com>
2021-04-11 15:30:51 +0300

    pre merge

--- client/src/components/Calendar/Calendare.jsx
+++ client/src/components/Calendar/Calendare.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function Calendare() {
   const style = {
     border: 0,
 
   }
+
+  useEffect(() => {
+    fetch(`https://www.googleapis.com/calendar/v3/calendars/uudmopujkodqksbu55au8opt3k@group.calendar.google.com/events`, {
+      headers: {
+        Authorization: 'Bearer ' + 'ya29.a0AfH6SMAiY4KhNsHMM0qse3wql30M513xRt2HllgKldOfTzd10M2Wr4uhJzHiQGTn7JnrV3ttvQ4SAyuIHeoY4jjoQzFP_yMJB4HHxlEcfYPhysTYGV-NY6nV0I4iARYq3lpTHbb5h0ElfPZ4RP3iYw72Mm_C',
+      },
+    }).then((data) => data.json()).then((data) => console.log(data))
+  }, [])
 
   return (
     <div>Здесь будет календарь</div>
--- server/config.js
+++ server/config.js
@@ -4,7 +4,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('./db/models/user');
 
 passport.serializeUser((user, done) => {
-  console.log(user);
+  // console.log(user);
   done(null, user._id);
 });
 passport.deserializeUser((id, done) => {
@@ -21,14 +21,25 @@ passport.use(
     callbackURL: '/api/v1/auth/google/redirect',
   }, (accessToken, refreshToken, profile, done) => {
     // check if user already exists in our own db
+    console.log('profile new user: ', profile)
     const googlEmail = profile.emails[0].value;
     User.findOne({ email: googlEmail }).then((currentUser) => {
       if (currentUser) {
         // already have this user
-        // console.log('user is: ', currentUser)
+        console.log('user is: ', currentUser)
+        console.log('accessToken', accessToken);
         return done(null, currentUser);
+      } else {
+        new User({
+          googleId: profile.id,
+          googleName: profile.displayName,
+          email: profile.emails[0].value,
+        }).save().then((newUser) => {
+          console.log('created new user: ', newUser)
+          return done(null, newUser);
+        })
       }
       return done(null, false, { message: 'Incorrect user' });
-    });
+      });
   }),
 );
