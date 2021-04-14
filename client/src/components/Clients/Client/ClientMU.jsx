585f96cac82554ed0c93ef86d323ddca0d9607e6
Anastasia <anastas1a982@icloud.com>
2021-04-14 17:23:31 +0300

    some test changes

--- client/src/components/Clients/Client/ClientMU.jsx
+++ client/src/components/Clients/Client/ClientMU.jsx
@@ -85,10 +85,10 @@ export default function ClientMU() {
     <Container maxWidth='lg'>
       <Grid container>
         <Grid item container xs={6} direction='column' className={classes.userCard}>
-        <div>
-          <Typography variant='h6'>Информация о клиенте</Typography>
+          <div>
+            <Typography variant='h6'>Информация о клиенте</Typography>
 
-        </div>
+          </div>
           <ButtonGroup>
             <Button color="inherit" component={RouterLink} to={`/clients/${client._id}/edit`}>Редактировать</Button>
             {user?.role === 'Admin' ? <Button color="inherit" onClick={deleteHandler}>Удалить клиента</Button> : null}
@@ -125,8 +125,8 @@ export default function ClientMU() {
             <TableBody>
               {client?.orders?.length
                 ? client.orders.map(order => (
-                  <TableRow key={order._id} className={ order.status === 'в работе' ? 
-                  classes.inWork : order.status === 'завершен' ? classes.finished : classes.claim }>
+                  <TableRow key={order._id} className={order.status === 'в работе' ?
+                    classes.inWork : order.status === 'завершен' ? classes.finished : classes.claim}>
                     <TableCell align='center'><Button component={RouterLink} to={`/orders/${order._id}`}>{order.number}</Button></TableCell>
                     <TableCell>{order.contractNumber}</TableCell>
                     <TableCell>{order.title}</TableCell>
@@ -139,20 +139,34 @@ export default function ClientMU() {
           </Table>
           <Button onClick={addOrderHandler} color="primary">Добавить заказ</Button>
         </Grid>
-        <Grid item container xs={6} direction='column' justify='space-between' style={{ minHeight: '700px' }}>
-          <Paper style={{ minHeight: '600px', overflowY: 'scroll' }} className={classes.userCard}>
-          <div>
-            <Typography variant='h6'>Комментарии по клиенту</Typography>
-
-          </div>
-            <ul>
-              {client?.comments?.length
+        <Grid  style={{ minHeight: '700px' }}>
+          <Paper style={{ minHeight: '600px', overflowY: 'scroll' }} >
+            <div>
+              <Typography variant='h6'>Комментарии по клиенту</Typography>
+            </div>
+            <Table>
+         
+            <TableBody>
+            {client?.comments?.length
                 ? client.comments.map(comment => (
-                  <li key={comment._id}>{`${comment.manager?.lastName} ${comment.manager?.name[0]}. ${comment.manager?.middleName[0]}.`} {new Date(comment.createdAt).toLocaleString()}: {comment.text}</li>
+                  <TableRow key={comment._id} >
+                    <TableCell>{`${comment.manager?.lastName} ${comment.manager?.name[0]}. ${comment.manager?.middleName[0]}.`} :</TableCell>
+                    <TableCell>{comment.text}</TableCell>
+                    <TableCell>{new Date(comment.createdAt).toLocaleString()}</TableCell>
+                  </TableRow>
                 ))
                 : null
               }
-            </ul>
+            </TableBody>
+          </Table>
+            {/* <ul>
+              {client?.comments?.length
+                ? client.comments.map(comment => (
+                  <li key={comment._id}>{`${comment.manager?.lastName} ${comment.manager?.name[0]}. ${comment.manager?.middleName[0]}.`} : {comment.text}{new Date(comment.createdAt).toLocaleString()}</li>
+                ))
+                : null
+              }
+            </ul> */}
           </Paper>
           <>
             {!loading ?
@@ -174,7 +188,7 @@ export default function ClientMU() {
           </>
         </Grid>
       </Grid>
-    </Container>
+    </Container >
   )
 }
 
