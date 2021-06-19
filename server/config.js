daba57ea55eb664e40336d6488767bc7dd0ea4fa
Irina <kisilirina@ya.ru>
2021-06-19 20:46:46 +0300

    readme

--- server/config.js
+++ server/config.js
@@ -7,47 +7,55 @@ const User = require('./db/models/user');
 passport.serializeUser((user, done) => {
   done(null, user._id);
 });
-passport.deserializeUser((id, done) => {
-  User.findById(id).then((user) => {
-    done(null, user);
-  });
+
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
 });
 
-const strategy = new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: `${process.env.OUR_REACT}/api/v1/auth/google/redirect`,
-  proxy: true,
-}, (accessToken, refreshToken, profile, done) => {
-  const googleEmail = profile.emails[0].value;
-  const photo = profile.photos[0].value;
-  const googleId = profile.id;
-  const googleName = profile.displayName;
-  let userForUpdate;
-  if (refreshToken) {
-    userForUpdate = {
-      accessToken,
-      refreshToken,
-      photo,
-      googleName,
-      googleId,
+const strategy = new GoogleStrategy(
+  {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: `${process.env.OUR_REACT}/api/v1/auth/google/redirect`,
+    proxy: true,
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    const googleEmail = profile.emails[0].value;
+    const photo = profile.photos[0].value;
+    const googleId = profile.id;
+    const googleName = profile.displayName;
+    let userForUpdate;
+
+    if (refreshToken) {
+      userForUpdate = {
+        accessToken,
+        refreshToken,
+        photo,
+        googleName,
+        googleId,
+      };
+    } else {
+      userForUpdate = {
+        accessToken,
+        photo,
+        googleName,
+        googleId,
+      };
     }
-  } else userForUpdate = {
-    accessToken,
-    photo,
-    googleName,
-    googleId,
-  }
 
-  User.findOneAndUpdate({ email: googleEmail },
-    userForUpdate, { new: true })
-    .then((currentUser) => {
-      if (currentUser && currentUser.canAccess) {
-        return done(null, currentUser);
-      }
-      return done(null, false, { message: 'User incorrect!' });
-    });
-});
+    const currentUser = await User.findOneAndUpdate(
+      { email: googleEmail },
+      userForUpdate,
+      { new: true }
+    );
+
+    if (currentUser && currentUser.canAccess) {
+      return done(null, currentUser);
+    }
+    return done(null, false, { message: 'User incorrect!' });
+  }
+);
 
 passport.use(strategy);
 refresh.use(strategy);
